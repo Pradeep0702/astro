6f892026e11a6aaf7a125c52526f78dc02127aaa
Pradeep <pradeepsinghjadaun7@gmail.com>
2025-02-09 21:47:12 +0530

    some issue

--- resources/views/admin/backlayout/footer.blade.php
+++ resources/views/admin/backlayout/footer.blade.php
@@ -1,4 +1,4 @@
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" ></script>
 @if(Route::currentRouteName() != 'back.auth')
 <script src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>
 <script src="https://cdn.datatables.net/2.0.7/js/dataTables.bootstrap5.js"></script>
