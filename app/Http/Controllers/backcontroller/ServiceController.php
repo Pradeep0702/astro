4f9399ff3b9953ae40733fc6d69e8662d414ae6b
Pradeep <pradeepsinghjadaun7@gmail.com>
2025-04-17 23:08:47 +0530

    bug fix

--- app/Http/Controllers/backcontroller/ServiceController.php
+++ app/Http/Controllers/backcontroller/ServiceController.php
@@ -85,7 +85,7 @@ class ServiceController extends Controller
                 $main_section[] = [
                     'title'=>$data['title']  ?? "",
                     'subtitle'=>$data['subtitle']  ?? "",
-                    'description' => isset($data['description']) ? nl2br($data['description']) : "",
+                    'description'=>$data['description']  ?? "",
                     'image'=>$filename
                 ]; 
             }
@@ -198,7 +198,7 @@ class ServiceController extends Controller
             $main_section[] = [
                 'title'=>$data['title'],
                 'subtitle'=>$data['subtitle'],
-                'description' => isset($data['description']) ? nl2br($data['description']) : "",
+                'description'=>$data['description'],
                 'image'=>$filename
             ]; 
         }             
