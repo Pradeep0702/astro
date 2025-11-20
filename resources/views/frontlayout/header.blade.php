b1336f5277a4c6dd6141c0b475169be8459a0046
Pradeep <pradeepsinghjadaun7@gmail.com>
2025-11-20 22:47:12 +0530

    changes code

--- resources/views/frontlayout/header.blade.php
+++ resources/views/frontlayout/header.blade.php
@@ -6,13 +6,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="google-site-verification" content="HhkOzn594eYQVIRs7rDuff-vAU0o8n-Kn-YvZ3Phi1Q" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title',$defaultTitle)</title>
     <meta name="description" content="@yield('description',$defaultDescription)"/> 
     <meta name="keywords" content="@yield('keywords',$defaultKeywords)"/>   
+    <meta name="google-site-verification" content="HhkOzn594eYQVIRs7rDuff-vAU0o8n-Kn-YvZ3Phi1Q" />
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"/>
